f5bf0423140988268acb8b6ac8e04ef648697a44
Baekgyu <centneuf0109@gmail.com>
2023-05-04 16:42:51 +0900

    add: authMiddleware.js

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,15 @@
+export const onlyisLoggedIn = (req, res, next) => {
+  if (req.session.isLoggedIn) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+export const onlyisLoggedOut = (req, res, next) => {
+  if (!req.session.isLoggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
+};
