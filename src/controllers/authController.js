3627bf4ea019e466aeef9990599fb14e1c530b6e
Baekgyu <centneuf0109@gmail.com>
2023-05-04 16:43:45 +0900

    ftr: initialize authController

---
+++ src/controllers/authController.js
@@ -0,0 +1,7 @@
+import User from "../models/User";
+import bcrypt from "bcrypt";
+
+const loginUserToSession = (req, user) => {
+  req.session.isLoggedIn = true;
+  req.session.loggedInUser = user;
+};
