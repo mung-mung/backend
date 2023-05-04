de381da51ab804b40a7605d6f4e83b55b94609a4
Baekgyu <centneuf0109@gmail.com>
2023-05-04 18:48:55 +0900

    rft: remove home URL

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -6,7 +6,6 @@ import express from "express";
 
 const rootRouter = express.Router();
 
-rootRouter.route("/").get((req, res) => res.send("Home"));
 rootRouter.use("/auth", authRouter);
 rootRouter.use("/user", userRouter);
 rootRouter.use("/match", matchRouter);
