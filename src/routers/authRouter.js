ea92c2337eb61b144224b723b10831bb13de1e2e
Baekgyu <centneuf0109@gmail.com>
2023-05-04 16:43:14 +0900

    ftr: configure authRouter

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,7 +1,13 @@
 import express from "express";
+import {postSignup, postLogin, getLogout} from "../controllers/authController";
+import {onlyisLoggedIn, onlyisLoggedOut} from "../middlewares/authMiddleware";
 
 const authRouter = express.Router();
 
 authRouter.route("/").get((req, res) => res.send("Auth"));
+
+authRouter.route("/signup").all(onlyisLoggedOut).post(postSignup);
+authRouter.route("/login").all(onlyisLoggedOut).post(postLogin);
+authRouter.route("/logout").all(onlyisLoggedIn).get(getLogout);
 
 export default authRouter;
