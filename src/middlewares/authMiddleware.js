c73c157e90a8ac8b2733832570630215166b8aff
Baekgyu <centneuf0109@gmail.com>
2023-05-04 18:48:29 +0900

    mod: authMiddleware.js

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -4,7 +4,11 @@ export const onlyisLoggedIn = (req, res, next) => {
   if (req.session.isLoggedIn) {
     return next();
   } else {
-    return res.redirect("/login");
+    return httpResponse.BAD_REQUEST(
+      res,
+      "로그인이 필요한 서비스입니다. 로그인 후 이용해주세요.",
+      "",
+    );
   }
 };
 
@@ -12,6 +16,10 @@ export const onlyisLoggedOut = (req, res, next) => {
   if (!req.session.isLoggedIn) {
     return next();
   } else {
-    return res.redirect("/");
+    return httpResponse.BAD_REQUEST(
+      res,
+      "이미 로그인된 상태입니다. 로그아웃 후 이용해주세요.",
+      "",
+    );
   }
 };
