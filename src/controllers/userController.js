debee7fd062a799e019e8c6bf8add1b9d435018c
Baekgyu <centneuf0109@gmail.com>
2023-05-07 20:46:21 +0900

    ftr: patchUserEdit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -9,3 +9,35 @@ export const getLoggedInUser = (req, res) => {
     return httpResponse.BAD_REQUEST(res, "", error);
   }
 };
+export const patchUserEdit = async (req, res) => {
+  const {_id, userType} = req.session.loggedInUser;
+  const {email, id, pw, avataUrl, contact, sex, birthYear} = req.body;
+
+  const exists = await User.exists({$or: [{id}, {email}]});
+  if (exists) {
+    return httpResponse.BAD_REQUEST(
+      res,
+      "같은 이메일 또는 아이디를 가진 계정이 이미 존재합니다. 다시 시도해주세요.",
+      "",
+    );
+  }
+  try {
+    const newUser = await User.findByIdAndUpdate(
+      _id,
+      {
+        userType,
+        email,
+        id,
+        pw,
+        avataUrl,
+        contact,
+        sex,
+        birthYear,
+      },
+      {new: true},
+    );
+    httpResponse.SUCCESS_OK(res, "유저 정보 수정 성공", newUser);
+  } catch (error) {
+    httpResponse.BAD_REQUEST(res, "", error);
+  }
+};
