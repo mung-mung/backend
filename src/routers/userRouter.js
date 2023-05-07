cbbf2acb24cf6925a7d09486c3d0904ac894a2df
Baekgyu <centneuf0109@gmail.com>
2023-05-07 20:45:17 +0900

    mod: userRouter.js

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,16 @@
 import ownerRouter from "./userRouters/ownerRouter";
 import walkerRouter from "./userRouters/walkerRouter";
 
+import {onlyisLoggedIn} from "../middlewares/authMiddleware";
+import {getLoggedInUser, patchUserEdit} from "../controllers/userController";
+
 import express from "express";
 
 const userRouter = express.Router();
 
-userRouter.route("/").get((req, res) => res.send("User"));
+userRouter.route("/").all(onlyisLoggedIn).get(getLoggedInUser);
+userRouter.route("/edit").all(onlyisLoggedIn).patch(patchUserEdit);
+
 userRouter.use("/owner", ownerRouter);
 userRouter.use("/walker", walkerRouter);
 
