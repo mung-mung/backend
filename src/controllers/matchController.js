27fc0705d1fc288e98b7635f6a5133d791e1fc9f
Baekgyu <centneuf0109@gmail.com>
2023-05-09 22:06:28 +0900

    ftr: getAllMatches

--- src/controllers/matchController.js
+++ src/controllers/matchController.js
@@ -1,6 +1,35 @@
+import Match from "../models/Match";
+import User from "../models/User";
+import Owner from "../models/Owner";
+import Walker from "../models/Walker";
+
 const {httpResponse} = require("../configs/httpResponse");
 
-export const getAllMatches = async (req, res) => {};
-export const postOneMatch = async (req, res) => {};
-export const getOneMatch = async (req, res) => {};
-export const deleteOneMatch = async (req, res) => {};
+export const getAllMatches = async (req, res) => {
+  try {
+    const loggedInUser = req.session.loggedInUser;
+    const loggedInUserType = loggedInUser.userType;
+    const userId = loggedInUser._id;
+    if (loggedInUserType === "owner") {
+      const loggedInOwner = await Owner.findOne({userId});
+      const ownerId = loggedInOwner._id;
+      const matches = await Match.find({ownerId});
+      return httpResponse.SUCCESS_OK(
+        res,
+        "현재 로그인 되어있는 owner에 대한 모든 match들 입니다.",
+        matches,
+      );
+    } else if (loggedInUserType === "walker") {
+      const loggedInWalker = await Walker.findOne({userId});
+      const walkerId = loggedInWalker._id;
+      const matches = await Match.find({walkerId});
+      return httpResponse.SUCCESS_OK(
+        res,
+        "현재 로그인 되어있는 walker에 대한 모든 match들 입니다.",
+        matches,
+      );
+    }
+  } catch (error) {
+    return httpResponse.BAD_REQUEST(res, "", error);
+  }
+};
