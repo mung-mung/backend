ece91a4c061a23ea2a312b95cc0bde91839fcece
Baekgyu <centneuf0109@gmail.com>
2023-05-09 20:09:27 +0900

    fix: Dog.js

--- src/models/Dog.js
+++ src/models/Dog.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 const dogSchema = new mongoose.Schema(
   {
-    ownerID: {
+    ownerId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
     dogName: {type: String, required: true},
-    birthday: {type: String, required: true},
+    birthYearMonth: {type: String, required: true},
     breed: {type: String, required: true},
     weight: {type: Number, required: true},
   },
