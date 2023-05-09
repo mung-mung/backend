d815c4bf4a59356e0835fb67b64c778d2dc77e6d
Baekgyu <centneuf0109@gmail.com>
2023-05-09 16:16:01 +0900

    ftr: getLoggedInOwner

--- src/controllers/userControllers/ownerController.js
+++ src/controllers/userControllers/ownerController.js
@@ -1,4 +1,14 @@
 const {httpResponse} = require("../../configs/httpResponse");
+import Owner from "../../models/Owner";
 
-export const getLoggedInOwner = async (req, res) => {};
-export const patchLoggedInOwner = async (req, res) => {};
+export const getLoggedInOwner = async (req, res) => {
+  const {_id} = req.session.loggedInUser;
+  try {
+    const loggedInOwner = await Owner.find({userId: _id});
+    return httpResponse.SUCCESS_OK(res, "", {
+      loggedInOwner,
+    });
+  } catch (error) {
+    return httpResponse.BAD_REQUEST(res, "", error);
+  }
+};
