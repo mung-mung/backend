7b808189a492211f4386d5a6a1ba26d6d4da23fb
Baekgyu <centneuf0109@gmail.com>
2023-05-09 16:16:46 +0900

    ftr: patchLoggedInWalker

--- src/controllers/userControllers/walkerController.js
+++ src/controllers/userControllers/walkerController.js
@@ -12,3 +12,24 @@ export const getLoggedInWalker = async (req, res) => {
     return httpResponse.BAD_REQUEST(res, "", error);
   }
 };
+export const patchLoggedInWalker = async (req, res) => {
+  const userId = req.session.loggedInUser._id;
+  const {greeting, availableTime, appeal, location} = req.body;
+  try {
+    const loggedInWalker = await Walker.findOne({userId});
+    const walkerId = loggedInWalker._id;
+    const newWalker = await Walker.findByIdAndUpdate(
+      walkerId,
+      {
+        greeting,
+        availableTime,
+        appeal,
+        location,
+      },
+      {new: true},
+    );
+    return httpResponse.SUCCESS_OK(res, "", newWalker);
+  } catch (error) {
+    return httpResponse.BAD_REQUEST(res, "", error);
+  }
+};
