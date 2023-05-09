27eaa9137e7d7d7822211ead7433dfbd590a8475
Baekgyu <centneuf0109@gmail.com>
2023-05-09 16:16:37 +0900

    ftr: getLoggedInWalker

--- src/controllers/userControllers/walkerController.js
+++ src/controllers/userControllers/walkerController.js
@@ -1,4 +1,14 @@
+import User from "../../models/User";
+import Walker from "../../models/Walker";
+
 const {httpResponse} = require("../../configs/httpResponse");
 
-export const getLoggedInWalker = async (req, res) => {};
-export const patchLoggedInWalker = async (req, res) => {};
+export const getLoggedInWalker = async (req, res) => {
+  const {_id} = req.session.loggedInUser;
+  try {
+    const loggedInWalker = await Walker.find({userId: _id});
+    return httpResponse.SUCCESS_OK(res, "", loggedInWalker);
+  } catch (error) {
+    return httpResponse.BAD_REQUEST(res, "", error);
+  }
+};
