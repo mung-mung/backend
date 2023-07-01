0d2a16f82f32fdd021aacba125585143bbebd14a
Baekgyu <centneuf0109@gmail.com>
2023-07-02 00:11:02 +0900

    fix: controller name error

--- src/routers/apiRouters/userRouters/ownerRouter.js
+++ src/routers/apiRouters/userRouters/ownerRouter.js
@@ -1,13 +1,13 @@
 import express from "express";
 import {onlyisLoggedIn} from "../../../middlewares/authMiddleware";
 import {
-  getLoggedInOwner,
+  getAllOwners,
   patchLoggedInOwner,
 } from "../../../controllers/userControllers/ownerController";
 
 const ownerRouter = express.Router();
 
 ownerRouter.use(onlyisLoggedIn);
-ownerRouter.route("/").get(getLoggedInOwner).patch(patchLoggedInOwner);
+ownerRouter.route("/").get(getAllOwners).patch(patchLoggedInOwner);
 
 export default ownerRouter;
--- src/routers/apiRouters/userRouters/walkerRouter.js
+++ src/routers/apiRouters/userRouters/walkerRouter.js
@@ -1,13 +1,13 @@
 import express from "express";
 import {onlyisLoggedIn} from "../../../middlewares/authMiddleware";
 import {
-  getLoggedInWalker,
+  getAllWalkers,
   patchLoggedInWalker,
 } from "../../../controllers/userControllers/walkerController";
 
 const ownerRouter = express.Router();
 
 ownerRouter.use(onlyisLoggedIn);
-ownerRouter.route("/").get(getLoggedInWalker).patch(patchLoggedInWalker);
+ownerRouter.route("/").get(getAllWalkers).patch(patchLoggedInWalker);
 
 export default ownerRouter;
